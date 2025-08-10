333a6aad21d33953c90b9a4a47896ed1554863a8
rakesh <rakesh@gmail.com>
2025-08-10 16:40:49 +0530

    web

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "156410157"
+    serviceWorkerVersion: "1575656990"
   }
 });
